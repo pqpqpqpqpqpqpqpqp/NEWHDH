1ec9bb6aa4fc428434a3305fdbd45cb54615c1c4
HDH <hong135689@gmail.com>
2025-04-29 15:38:24 +0900

    ㅇㅅㅇ

--- JAVA/git_test.java
+++ JAVA/git_test.java
@@ -2,7 +2,8 @@ package JAVA;
 
 public class git_test {
 	public static void main(String[] args) {
-		System.out.println("???");
-		System.out.println("!!!");
+		System.out.println("자바로 무엇을 해야하는가");
+		System.out.println("자바로 무엇을 할수있는가");
+		System.out.println("진짜 뭐함");
 	}
 }
